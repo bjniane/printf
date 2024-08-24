d77c70e14d83ffe674fa29a10dc771aa5fe9cb84
bjniane <bjniane@e1r6p12.1337.ma>
2024-08-24 03:32:41 +0100

    update

--- put_str.c
+++ put_str.c
@@ -6,7 +6,7 @@
 /*   By: bjniane <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:42:16 by bjniane           #+#    #+#             */
-/*   Updated: 2023/11/29 15:52:11 by bjniane          ###   ########.fr       */
+/*   Updated: 2024/08/24 03:32:05 by bjniane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	ft_strlen(const char *str)
 	if (!str)
 		return (0);
 	i = 0;
-	while (str)
+	while (str[i])
 		i++;
 	return (i);
 }
@@ -28,7 +28,7 @@ int	put_str(char *s)
 {
 	if (s == NULL)
 	{
-		write(1, "null", 4);
+		write(1, "(null)", 6);
 		return (6);
 	}
 	else
